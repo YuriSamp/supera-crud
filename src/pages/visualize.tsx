30fed630c92faca22571c5cbfa4c8fed6a67ac8e
YuriSamp <yurisamp123@gmail.com>
2023-10-02 11:25:17 -0300

    add visualize page

--- src/pages/visualize.tsx
+++ src/pages/visualize.tsx
@@ -1,49 +1,41 @@
-import { HStack, Heading, Input, Button, Select } from '@chakra-ui/react'
+import { Heading, Input, VStack } from '@chakra-ui/react'
+import { useAtomValue } from 'jotai';
 import { useParams } from 'react-router-dom';
-import { toast, ToastContainer } from 'react-toastify';
-import { formSchema } from '../lib/schema/form';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import * as yup from 'yup'
+import { userAtom } from '../lib/context';
+import Navbar from '../components/navbar';
 
 const Visualize = () => {
 
+  const users = useAtomValue(userAtom)
   const { id } = useParams()
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<yup.InferType<typeof formSchema>>()
-
-
-  const onSubmit: SubmitHandler<yup.InferType<typeof formSchema>> = (data) => {
-    console.log(data)
-    toast.success('Teste')
-  }
-
+  const user = users.filter(user => user.id === id)[0]
 
   return (
-    <>
-      <ToastContainer />
-      <HStack height='calc(100vh)' bg={'ghostwhite'} flexDir={'column'} placeItems={'center'} gap={20} paddingX={'96'}>
-        <HStack>
-          <Heading>
-            Teste
-          </Heading>
-          <HStack flexDir={'column'} justifyContent={'center'} placeItems={'center'} gap={'3'}>
-            <Input placeholder='Nome' />
-            <Input placeholder='Email' />
-            <Select placeholder='Selecione o perfil' >
-              <option value='Administrador'>Administrador</option>
-              <option value='Usuário Comum'>Usuário Comum</option>
-            </Select>
-            <Input placeholder='Telefone' />
-            <Input placeholder='Idade' />
-            <Button type='submit'>Adicionar usuário</Button>
-          </HStack>
-        </HStack>
-      </HStack>
-    </>
+    <VStack height='calc(100vh)' bg={'ghostwhite'} flexDir={'column'} placeItems={'center'} gap={40} paddingX={'96'}>
+      <Navbar />
+      <VStack flexDir={'column'} gap={4} borderWidth={1} borderColor={'black'} padding={6} rounded={'md'} className='brutalism-box'>
+        <Heading fontSize={26}>
+          Informações do usuário
+        </Heading>
+        <VStack flexDir={'column'} justifyContent={'center'} placeItems={'center'} gap={'3'}>
+          <VStack flexDir={'column'} gap={'2'} w={330}>
+            <Input value={user.nome} />
+          </VStack>
+          <VStack flexDir={'column'} gap={'2'} w={330}>
+            <Input value={user.email} />
+          </VStack>
+          <VStack flexDir={'column'} gap={'2'} w={330}>
+            <Input value={user.perfil} />
+          </VStack >
+          <VStack flexDir={'column'} gap={'2'} w={330}>
+            <Input value={user.telefone?.length ? user.telefone : 'Não informado'} />
+          </VStack>
+          <VStack flexDir={'column'} gap={'2'} w={330}>
+            <Input value={user.idade ? user.idade : 'Não informado'} />
+          </VStack>
+        </VStack>
+      </VStack>
+    </VStack>
   )
 }
 
