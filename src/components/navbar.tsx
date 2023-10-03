6e9a22b310bb5d8dbdae80596144b0056149e022
YuriSamp <yurisamp123@gmail.com>
2023-10-03 17:51:51 -0300

    refactoring navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,21 @@
 import { HStack, Link, Heading } from '@chakra-ui/react'
 import { Link as RouterLink } from 'react-router-dom'
+
+const RoutesList = [
+  { to: '/', label: 'Listagem' },
+  { to: '/create', label: 'Cadastro' }
+]
+
 const Navbar = () => {
   return (
     <HStack justifyContent={'space-between'} py={4} borderBottomWidth={1} placeItems={'center'} borderColor={'black'} w={'full'}>
       <Heading fontSize={24}>Teste Técnico</Heading>
       <HStack gap={6}>
-        <Link as={RouterLink} to={'/'}>
-          Listagem
-        </Link>
-        <Link as={RouterLink} to={'/create'}>
-          Cadastro
-        </Link>
+        {RoutesList.map(route => (
+          <Link as={RouterLink} to={route.to}>
+            {route.label}
+          </Link>
+        ))}
       </HStack>
     </HStack>
   )
